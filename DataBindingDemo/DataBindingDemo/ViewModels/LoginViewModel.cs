ac345dcf8c85990951636b95877f6e5e71106bb6
Thomas Galliker <thomas@bluewin.ch>
2023-04-14 16:40:47 +0200

    Add sample code

--- DataBindingDemo/DataBindingDemo/ViewModels/LoginViewModel.cs
+++ DataBindingDemo/DataBindingDemo/ViewModels/LoginViewModel.cs
@@ -77,6 +77,7 @@ namespace DataBindingDemo.ViewModels
             //           }
             //           else
             //           {
+            //               this.Password = string.Empty;
             //               this.dialogService.ShowAlert("Username or password is not valid.");
             //           }
             //       }
