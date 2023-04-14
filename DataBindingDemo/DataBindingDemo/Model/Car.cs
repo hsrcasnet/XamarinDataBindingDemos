55e4e1c570ba9d91fd5a2707bf542c78870975d7
Thomas Galliker <thomas@bluewin.ch>
2023-04-14 16:39:53 +0200

    Remove tostring

--- DataBindingDemo/DataBindingDemo/Model/Car.cs
+++ DataBindingDemo/DataBindingDemo/Model/Car.cs
@@ -5,10 +5,5 @@
         public string Brand { get; set; }
 
         public string Model { get; set; }
-
-        public override string ToString()
-        {
-            return $"{{{this.Brand} {this.Model}}}";
-        }
     }
 }
